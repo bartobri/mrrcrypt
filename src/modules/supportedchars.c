582a8c4521ffa6fd22c1ee766539d576fe83d160
Brian Barto <bartobrian@gmail.com>
2016-09-29 09:44:08 -0400

    Create supportedchars module
    	new file:   src/modules/supportedchars.c
    	new file:   src/modules/supportedchars.h

---
+++ src/modules/supportedchars.c
@@ -0,0 +1,7 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
+#define SUPPORTED_CHARS  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]|\\:;\"'<>,.?/~\n\t "
+
---
+++ src/modules/supportedchars.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
+#ifndef SUPPORTEDCHARS_H
+#define SUPPORTEDCHARS_H 1
+
+
+
+#endif
