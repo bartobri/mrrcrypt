bc11389ea710565000ebd85880091d3bd89267f2
Brian Barto <bartobrian@gmail.com>
2016-11-22 18:21:24 -0500

    Add stats to show
    	modified:   src/show.c

--- src/show.c
+++ src/show.c
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[]) {
 	int i, ch;
+	int high = 0, low = 0, diff = 0;
 	int data[GRID_SIZE*4];
 	
 	memset(data, 0, sizeof(data));
@@ -15,8 +16,24 @@ int main(int argc, char *argv[]) {
 	}
 	
 	for (i = 0; i < GRID_SIZE*4; ++i) {
+		if (data[i] > high) {
+			high = data[i];
+		}
+		if (data[i] < low) {
+			low = data[i];
+		}
+	}
+	low = high;
+	for (i = 0; i < GRID_SIZE*4; ++i) {
+		if (data[i] < low) {
+			low = data[i];
+		}
+	}
+	diff = high - low;
+	for (i = 0; i < GRID_SIZE*4; ++i) {
 		printf("%i => %i\n", i, data[i]);
 	}
+	printf("%i -> %i, diff: %i\n", low, high, diff);
 
 	return 0;
 }
