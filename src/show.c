4bc4c9753b5c231991a00c72916e7a0fdc7192f5
Brian Barto <bartobrian@gmail.com>
2016-11-23 10:14:38 -0500

    Update show to take command option for number of bytes to test
    	modified:   src/show.c

--- src/show.c
+++ src/show.c
@@ -1,19 +1,57 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
 
+void show_stats(void);
+
+static int data[GRID_SIZE*4];
+
 int main(int argc, char *argv[]) {
-	int i, ch;
-	int high = 0, low = 0, diff = 0;
-	int data[GRID_SIZE*4];
+	int ch, c = 0, o;
+	int sample = 0;
 	
 	memset(data, 0, sizeof(data));
+	
+	// Check arguments
+	while ((o = getopt(argc, argv, "s:")) != -1) {
+		switch (o) {
+			case 's':
+				sample = atoi(optarg);
+				break;
+			case '?':
+				if (isprint(optopt))
+					fprintf (stderr, "Unknown option '-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character '\\x%x'.\n", optopt);
+		}
+	}
 
 	// Loop over input one char at a time and encrypt
 	while ((ch = getchar()) != EOF) {
 		(data[ch])++;
+		++c;
+		
+		if (sample > 0 && sample == c) {
+			show_stats();
+			c = 0;
+		}
 	}
+	
+	if (c) {
+		show_stats();
+	}
+
+	return 0;
+}
+
+void show_stats(void) {
+	int i;
+	int high = 0, low = 0, diff = 0;
+	float diffpct;
 	
 	for (i = 0; i < GRID_SIZE*4; ++i) {
 		if (data[i] > high) {
@@ -31,9 +69,12 @@ int main(int argc, char *argv[]) {
 	}
 	diff = high - low;
 	for (i = 0; i < GRID_SIZE*4; ++i) {
-		printf("%i => %i\n", i, data[i]);
+		//printf("%i => %i\n", i, data[i]);
 	}
-	printf("%i -> %i, diff: %i\n", low, high, diff);
-
-	return 0;
+	
+	diffpct = ((float)diff / (float)high) * 100;
+	
+	printf("%i -> %i, diff: %i (%%%.2f)\n", low, high, diff, diffpct);
+	
+	memset(data, 0, sizeof(data));
 }
