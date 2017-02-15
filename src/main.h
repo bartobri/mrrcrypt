7db07a3bae08fc94f95f73f81d66ae9b3ca36676
Brian Barto <bartobrian@gmail.com>
2017-02-14 20:28:06 -0500

    Update version number
    	modified:   src/main.h

--- src/main.h
+++ src/main.h
@@ -9,7 +9,7 @@
 /*
  * Program version number. Enumerated when the -v command option is used.
  */
-#define VERSION                "0.1.0"
+#define VERSION                "0.2.0"
 
 /*
  * The DEFAULT_KEY_NAME is the name of the default key that myycrypt will
