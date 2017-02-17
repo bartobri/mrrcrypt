6a5798b17152f5feb481d77bda78f571de66a136
Brian Barto <bartobrian@gmail.com>
2017-02-17 13:27:25 -0500

    Update license headers for GPL
    	modified:   src/main.c
    	modified:   src/main.h
    	modified:   src/modules/base64.c
    	modified:   src/modules/base64.h
    	modified:   src/modules/keyfile.c
    	modified:   src/modules/keyfile.h
    	modified:   src/modules/mirrorfield.c
    	modified:   src/modules/mirrorfield.h

--- src/modules/mirrorfield.c
+++ src/modules/mirrorfield.c
@@ -1,7 +1,7 @@
 // Copyright (c) 2016 Brian Barto
 //
 // This program is free software; you can redistribute it and/or modify it
-// under the terms of the MIT License. See LICENSE for more details.
+// under the terms of the GPL License. See LICENSE for more details.
 
 #include <stdio.h>
 #include <string.h>
